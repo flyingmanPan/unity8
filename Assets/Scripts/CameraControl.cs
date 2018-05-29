f79719c7a754b40deb1630a725d0c72cafc32554
ZZFM <flyingman_pan@outlook.com>
2018-05-29 23:42:46 +0800

    init

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+    public Transform target;
+    public float distanceUp = 10f;
+    public float distanceAway = 10f;
+    public float smooth = 2f;
+    public float camdepthsmooth = 5f;
+	// Use this for initialization
+	void Start () {
+        target = GameObject.Find("Car").GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if((Input.mouseScrollDelta.y<0&&Camera.main.fieldOfView>=3)||
+             Input.mouseScrollDelta.y > 0 && Camera.main.fieldOfView <= 80)
+        {
+            Camera.main.fieldOfView += Input.mouseScrollDelta.y * camdepthsmooth * Time.deltaTime;
+        }
+    }
+    private void LateUpdate()
+    {
+        Vector3 disPos = target.position + Vector3.up * distanceUp - target.forward * distanceAway;
+        transform.position = Vector3.Lerp(transform.position, disPos, Time.deltaTime * smooth);
+        transform.LookAt(target.position);
+    }
+}
---
+++ Assets/Scripts/smokeControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class smokeControl : MonoBehaviour {
+
+    public float engineRevs;
+    public float hSliderValue1 = 50.0f;
+    public float hSliderValue2 = 50.0f;
+    ParticleSystem exhaust;
+
+
+    void Start()
+    {
+        exhaust = GetComponent<ParticleSystem>();
+    }
+
+
+    void Update()
+    {
+        var emission = exhaust.emission;
+        emission.rateOverTime = hSliderValue1;
+        emission.rateOverDistance = hSliderValue2;
+    }
+    private void OnGUI()
+    {
+        //hSliderValue1 = GUI.HorizontalSlider(new Rect(25, 45, 100, 30), hSliderValue1, 5.0f, 200.0f);
+        //hSliderValue2 = GUI.HorizontalSlider(new Rect(25, 70, 100, 30), hSliderValue2, 5.0f, 200.0f);
+    }
+}
